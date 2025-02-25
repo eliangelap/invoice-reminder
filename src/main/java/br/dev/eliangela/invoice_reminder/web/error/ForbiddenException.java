f99f948e9a66f90a0d48dcf22db5cc864c4089f8
Eliangela M Palharini Paes <emenezes@coamo.com.br>
2025-02-24 23:05:58 -0300

    feat: 🎸 Initial commit
    Initial commit

---
+++ src/main/java/br/dev/eliangela/invoice_reminder/web/error/ForbiddenException.java
@@ -0,0 +1,15 @@
+package br.dev.eliangela.invoice_reminder.web.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/test/java/br/dev/eliangela/invoice_reminder/InvoiceReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.eliangela.invoice_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
